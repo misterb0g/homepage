d738c3dd228e8978074d09a77c30467ef57a1c67
misterb0g <74599242+misterb0g@users.noreply.github.com>
2025-09-02 21:08:38 +0200

    Update bookmarks.js
    Reorganisation

--- bookmarks.js
+++ bookmarks.js
@@ -8,62 +8,69 @@ const bookmarks = [
       { name: "My Actiris", url: "https://my.actiris.brussels/subsidized-jobs/dashboard/employeur/" },
     ],
   },
-    {
-    title: "Gestion",
+
+  {
+    title: "Comptes",
     links: [
-      { name: "Dolibarr", url: "https://dolibarr.lesilex.be" },      
       { name: "ING", url: "https://ebanking.ing.be/login" },
       { name: "Winbooks", url: "https://app.winbooksconnect.be/" },
+      { name: "Clé", url: "https://airkey.evva.com" },
     ],
   },
-  {
-    title: "Admin",
-    links: [
-      { name: "Odoo", url: "https://silex-asbl.odoo.com/odoo" },
-      { name: "Silex", url: "http://lesilex.be/admin/login.php" },
-      { name: "Flow", url: "https://admin.joinflow.com/pbxservices.jsp" },
-      { name: "Clés", url: "https://airkey.evva.com" },
-    ],
-  },
+
   {
     title: "Google",
     links: [
-    { name: "Drive Partagé", url: "https://drive.google.com/drive/shared-drives" },
-    { name: "Drive perso", url: "https://drive.google.com/drive/my-drive" },
+      { name: "Drive Partagé", url: "https://drive.google.com/drive/shared-drives" },
+      { name: "Drive perso", url: "https://drive.google.com/drive/my-drive" },
       { name: "Mail", url: "https://mail.google.com/mail" },
       { name: "Agenda", url: "https://calendar.google.com" },
     ],
   },
+
+  {
+    title: "Admin",
+    links: [
+      { name: "Dolibarr", url: "https://dolibarr.lesilex.be" },
+      { name: "Flow", url: "https://admin.joinflow.com/pbxservices.jsp" },
+      { name: "Odoo", url: "https://silex-asbl.odoo.com/odoo" },
+      { name: "Silex", url: "http://lesilex.be/admin/login.php" },
+    ],
+  },
+
   {
     title: "Video",
     links: [
       { name: "Prime", url: "https://www.primevideo.com" },
       { name: "Netflix", url: "https://netflix.com" },
-      {name: "Disney +", url: "https://www.disneyplus.com/fr-fr/home" },
-      {name: "Canal+", url: "https://pass.canalplus.com/" },
+      { name: "Disney +", url: "https://www.disneyplus.com/fr-fr/home" },
+      { name: "Canal+", url: "https://pass.canalplus.com/" },
     ],
   },
+
   {
     title: "News",
     links: [
       { name: "Club MacG", url: "https://clubigen.fr" },
       { name: "Courrier Inter", url: "https://www.courrierinternational.com" },
       { name: "Le Soir", url: "https://www.lesoir.be" },
-      { name: "RTBF", url: "https://www.rtbf.be" }
+      { name: "RTBF", url: "https://www.rtbf.be" },
     ],
   },
-    {
+
+  {
     title: "Sport",
     links: [
       { name: "Strava", url: "https://www.strava.com/login" },
       { name: "Garmin", url: "https://connect.garmin.com/modern/" },
-      { name: "Smashrun", url: "https://fr.smashrun.com/gilles.bogarts" },      
+      { name: "Smashrun", url: "https://fr.smashrun.com/gilles.bogarts" },
     ],
   },
+
   {
     title: "Web",
     links: [
-      { name: "Bog", url: "http://bogarts.be/wp-admin" },
+      { name: "Silex", url: "http://lesilex.be/admin/login.php" },
       { name: "Fleur d'Ô", url: "https://fleurdo.be/wp-login.php?redirect_to=https%3A%2F%2Ffleurdo.be%2Fwp-admin%2F&reauth=1" },
       { name: "Molinaux", url: "https://molineaux-flowers.be/wp-admin/" },
     ],
