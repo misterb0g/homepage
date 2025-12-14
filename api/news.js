94b470f1b1cf5a965d66fd408bfb7794f0b3c53f
misterb0g <74599242+misterb0g@users.noreply.github.com>
2025-12-14 19:15:39 +0100

    Update news.js

--- api/news.js
+++ api/news.js
@@ -2,7 +2,7 @@ import Parser from 'rss-parser';
 
 export default async function handler(request, response) {
   // Le flux principal et sécurisé de MacGeneration (souvent le plus fiable)
-  const targetUrl = 'https://cdn.mgig.fr/rss/light_rss_macg.xml'; 
+  const targetUrl = 'https://megaflux.macg.co/'; 
   
   // Configuration du parser avec les options pour contourner les protections
   const parser = new Parser({
